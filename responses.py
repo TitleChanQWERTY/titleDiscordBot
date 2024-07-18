099e0748cc2b8f0438c7e28fcbc042424a2327a1
TitleChanQWERTY <titlechanqwerty@gmail.com>
2024-07-18 19:01:37 +0200

    Add json func!

--- responses.py
+++ responses.py
@@ -1,25 +1,13 @@
 from random import choice, randint
+import json
 
 
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
-    if lowered == '':
-        return 'Well, you\'re awfully silent...'
-    elif 'привіт' in lowered:
-        return 'вітання!'
-    elif 'як справи' in lowered:
-        return 'все добре!'
-    elif 'гарного вечора' in lowered:
-        return 'завтра побачимося!!'
-    elif 'коли відео?' in lowered:
-        return 'колись'
-    elif '! кинути кубік' in lowered:
-        return f'випало: {randint(1, 10)}'
-    elif 'Слава україні' in lowered:
-        return "героям слава!"
-    elif '! посилання' in lowered:
-        return "**ютуб** https://www.youtube.com/@titleyakumo"
+    with open("response.json", 'r', encoding='utf-8') as res:
+        json_data = json.load(res)
 
-
-
+    for key, values, in json_data.items():
+        if key in lowered:
+            return values
